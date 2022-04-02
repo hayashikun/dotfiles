00488833860f7c84c3bc6b85aea55e318e712966
Ryosuke Hayashi <hayashi@hayashikun.com>
2022-04-02 22:51:51 +0900

    fix setup-go

--- setup-go.fish
+++ setup-go.fish
@@ -4,30 +4,22 @@ cd (dirname (status -f)) && source init.fish
 
 set GO_VERSION "1.17.7"
 
-if not test -d $HOME/.nodenv
-    git clone https://github.com/nodenv/nodenv.git $HOME/.nodenv
+switch (uname -m)
+    case "arm64"
+        set ARCH "arm64"
+    case "*"
+        set ARCH "amd64"
 end
-cd $HOME/.nodenv && git pull && src/configure && make -C src
-
-fish_add_path $HOME/.nodenv/bin
-source-config
-
-if not test -d $HOME/.nodenv/plugins/node-build
-    git clone https://github.com/nodenv/node-build.git $HOME/.nodenv/plugins/node-build
+set ARC (string join "" "go" $GO_VERSION "." (string lower (uname -s)) "-" $ARCH ".tar.gz")
+cd $CACHE_PATH
+if not test -f $ARC
+    curl "https://dl.google.com/go/$ARC" -O
+    sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf $ARC
 end
-cd $HOME/.nodenv/plugins/node-build && git pull
-
-if not test -d $HOME/.nodenv/plugins/node-build-update-defs
-    git clone https://github.com/nodenv/node-build-update-defs.git $HOME/.nodenv/plugins/node-build-update-defs
-end
-cd $HOME/.nodenv/plugins/node-build-update-defs && git pull
-
-nodenv install $NODE_VERSION -s && nodenv global $NODE_VERSION
+fish_add_path "/usr/local/go/bin"
+fish_add_path "$HOME/go/bin"
 
 cd $DOT_PATH
-for p in (cat npm-packages)
-    if test -n $p
-        npm install -g $p
-    end
+for p in (cat go-packages)
+    go install $p
 end
-
