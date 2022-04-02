f4a76d253ed28aa24a3be8ffdc6ff8d9bc6b665f
Ryosuke Hayashi <hayashi@hayashikun.com>
2022-04-02 16:42:13 +0900

    remove haskell installation from setup.fish

--- setup.fish
+++ setup.fish
@@ -145,19 +145,6 @@ function go-install
 end
 
 
-function haskell-install
-    if not test -d $HOME/.ghcup
-        set -x BOOTSTRAP_HASKELL_NONINTERACTIVE 1
-        set -x BOOTSTRAP_HASKELL_INSTALL_HLS 1
-        set -x BOOTSTRAP_HASKELL_INSTALL_STACK 1
-        curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
-    end
-    fish_add_path ~/.ghcup/bin
-    fish_add_path ~/.cabal/bin
-    ghcup upgrade
-end
-
-
 for a in $argv
     switch $a
         case "--https-repo"
@@ -260,8 +247,6 @@ end
 rust-install
 
 node-install
-
-haskell-install
 
 go-install
 
