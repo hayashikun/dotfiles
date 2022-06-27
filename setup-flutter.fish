cd04c24b22d487fb3b8a86aed6773a329d0c7a0e
Ryosuke Hayashi <hayashi@hayashikun.com>
2022-06-27 15:36:31 +0900

    fix setup-flutter

--- setup-flutter.fish
+++ setup-flutter.fish
@@ -8,7 +8,7 @@ set FLUTTER_PATH $HOME/flutter
 if not test -d $FLUTTER_PATH
     git clone git@github.com:flutter/flutter.git -b stable $FLUTTER_PATH
 end
-cd $FLUTTER_PATH && git pull origin stable
+cd $FLUTTER_PATH && git fetch origin stable
 git checkout $FLUTTER_VERSION
 
 fish_add_path $FLUTTER_PATH/bin
