134861321cdb616e84202f18892edaa4a702337e
Ryosuke Hayashi <hayashi@hayashikun.com>
2024-02-05 13:58:14 +0900

    awscli

--- setup-awscli.fish
+++ setup-awscli.fish
@@ -5,6 +5,11 @@ cd (dirname (status -f)) && source init.fish
 cd $CACHE_PATH
 
 curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
-
 sudo installer -pkg ./AWSCLIV2.pkg -target /
 
+# TODO (hayashikun): mac only 
+if is-mac
+    curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac_arm64/sessionmanager-bundle.zip" -o "sessionmanager-bundle.zip"
+    unzip sessionmanager-bundle.zip
+    sudo ./sessionmanager-bundle/install -i /usr/local/sessionmanagerplugin -b /usr/local/bin/session-manager-plugin
+end
